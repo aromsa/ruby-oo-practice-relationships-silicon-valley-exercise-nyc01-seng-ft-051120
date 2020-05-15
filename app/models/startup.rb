b2ba72132127cb71121cc96dd9c8998c8d50d29b
aromsa <annacbateman@gmail.com>
2020-05-15 15:11:29 -0400

    models

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,47 @@
 class Startup
 
+  attr_accessor :name
+  attr_reader :founder, :name, :domain
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+  end
+
+  def pivot=(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find do |startup|
+      startup.founder == founder_name
+  end
+  
 end
+
+
+# `Startup#name`
+#   - returns a **string** that is the startup's name
+# - `Startup#founder`
+#   - returns a **string** that is the founder's name
+#   - Once a startup is created, the founder cannot be changed.
+# - `Startup#domain`
+#   - returns a **string** that is the startup's domain
+# - `Startup#pivot`
+#   - given a string of a **domain** and a string of a **name**, change the domain
+#     and name of the startup. This is the only public method through which the
+#     domain should be changed.
+# - `Startup.all`
+#   - should return **all** of the startup instances
+# - `Startup.find_by_founder`
+#   - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+# - `Startup.domains`
+#   - should return an **array** of all of the different startup domains
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,12 @@
 class VentureCapitalist
 
 end
+
+# - `VentureCapitalist#name`
+#   - returns a **string** that is the venture capitalist's name
+# - `VentureCapitalist#total_worth`
+#   - returns a **number** that is the total worth of this investor (e.g., think of it as how much money they have)
+# - `VentureCapitalist.all`
+#   - returns an array of all venture capitalists
+# - `VentureCapitalist.tres_commas_club`
+#   - returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 `total_worth`)
